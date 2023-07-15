a03e929a79d9f2fb4541206e15d74040cfdd4f24
jnrsai24 <jnrsai24@gmail.com>
2023-07-15 14:32:50 +0000

    sidler now diplayed ,test cases all passed and CSS PENDING

--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -6,25 +6,16 @@ import './index.css'
 const PlanetSlider = props => {
   const {planetsList} = props
   console.log(planetsList)
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  }
+
   return (
     <div data-testid="planets" className="app-container">
-      <div className="bg-container">
-        <h1>PLANETS</h1>
-        <div className="slider-container ">
-          <Slider {...settings}>
-            {planetsList.map(each => (
-              <PlanetItem key={each.id} data={each} />
-            ))}
-          </Slider>
-        </div>
-      </div>
+      <h1>PLANETS</h1>
+
+      <Slider>
+        {planetsList.map(each => (
+          <PlanetItem key={each.id} data={each} />
+        ))}
+      </Slider>
     </div>
   )
 }
